c0bea55d460e71f712c56e5ce0e8dec4f67ec369
Wesley Bortoloso <wesleybortolosocco@gmail.com>
2025-04-15 10:38:59 -0300

    update authorization enum status

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -3,9 +3,9 @@ class Authorization < ApplicationRecord
   belongs_to :parent, class_name: 'Parent'
 
   enum :status, {
-    pending: 'Pendente',
-    approved: 'Aprovada',
-    denied: 'Recusada'
+    pending: 0,
+    approved: 1,
+    denied: 2
   }
 
   validates :date, :description, :status, presence: true

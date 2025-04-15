66056fe3d94bb5a841536c0bd9a8397cfe8a69d9
Wesley Bortoloso <wesleybortolosocco@gmail.com>
2025-04-15 07:08:44 -0300

    create student class

---
+++ app/models/student.rb
@@ -0,0 +1,13 @@
+class Student < User
+  jsonb_accessor :extra_info,
+                 enrollment: :string,
+                 external_id: :integer,
+                 situation: :string
+
+  validates :enrollment, :situation, presence: true
+
+  belongs_to :classroom, optional: true
+  has_many :occurrencies, dependent: :nullify, inverse_of: :student
+  has_many :authorizations, dependent: :destroy
+  has_many :orientations, dependent: :destroy
+end

c5e4187be094183bbb11f1b5264aa349a96f02fb
Wesley Bortoloso <wesleybortolosocco@gmail.com>
2025-04-19 12:18:37 -0300

    include ransack methods

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -6,4 +6,12 @@ class Teacher < User
 
   has_many :classrooms_teachers, dependent: :destroy
   has_many :classrooms, through: :classrooms_teachers
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name email document]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[classrooms classrooms_teachers]
+  end  
 end

0401163aef09caf7038a95dff67b6bc21dea0fd0
Wesley Bortoloso <wesleybortolosocco@gmail.com>
2025-04-19 12:18:23 -0300

    include apis in base

--- app/api/v1/base.rb
+++ app/api/v1/base.rb
@@ -2,5 +2,13 @@ module V1
   class Base < Grape::API
     mount V1::Auth
     mount V1::Occurrencies
+    mount V1::Students
+    mount V1::Parents
+    mount V1::Staffs
+    mount V1::Teachers
+    mount V1::Authorizations
+    mount V1::Schedules
+    mount V1::Orientations
+    mount V1::Classrooms
   end
 end

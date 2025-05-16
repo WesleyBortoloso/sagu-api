a104c55b4cfd7b8ec3e155d7105d4f6691aa6464
WesleyBortoloso <wesleybortolosocco@gmail.com>
2025-05-16 18:01:14 -0300

    update sdocument student class_name

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,5 @@
 class Document < ApplicationRecord
-  belongs_to :student, class_name: 'User', foreign_key: :student_id
+  belongs_to :student, class_name: 'Student', foreign_key: :student_id
   has_one_attached :file
 
   validates :name, :kind, presence: true

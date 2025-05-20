85704f206e633b288d8ad554e0ee01ba02b2fe9d
WesleyBortoloso <wesleybortolosocco@gmail.com>
2025-05-20 11:18:37 -0300

    update event factory

--- app/factories/update_event_description_factory.rb
+++ app/factories/update_event_description_factory.rb
@@ -2,13 +2,13 @@ class UpdateEventDescriptionFactory
   def self.call(resource:, changed_fields:)
     case changed_fields.first
     when "status"
-      "O status foi atualizado -> #{resource.status.humanize}"
+      "O status da(o) #{t_enum(resource, :title)} foi atualizado: #{t_enum(resource, :status)}"
     when "area"
-      "A area envolvida foi atualizada -> #{resource.area.humanize}"
+      "A area da(o) #{t_enum(resource, :title)} foi atualizada: #{t_enum(resource, :area)}"
     when "responsible_id"
-      "O responsável foi atualizado -> #{resource.responsible.name}"
+      "O responsável foi atualizado: #{resource.responsible.name}"
     when "severity"
-      "A gravidade foi atualizada -> #{resource.severity.humanize}"
+      "A gravidade da(o) #{t_enum(resource, :title)} foi atualizada: #{t_enum(resource, :severity)}"
     else
       "Uma atualização foi realizada"
     end

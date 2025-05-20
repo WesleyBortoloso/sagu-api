daf9253b0286aaa203835888adbfe9686f71c7ae
Wesley Bortoloso <wesleybortolosocco@gmail.com>
2025-05-20 13:50:05 -0300

    update translation model

--- app/factories/update_event_description_factory.rb
+++ app/factories/update_event_description_factory.rb
@@ -1,18 +1,20 @@
 class UpdateEventDescriptionFactory
-  include TranslationHelper
+  class << self
+    include TranslationHelper
 
-  def self.call(resource:, changed_fields:)
-    case changed_fields.first
-    when "status"
-      "O status da(o) #{t_enum(resource, :title)} foi atualizado: #{t_enum(resource, :status)}"
-    when "area"
-      "A area da(o) #{t_enum(resource, :title)} foi atualizada: #{t_enum(resource, :area)}"
-    when "responsible_id"
-      "O responsável foi atualizado: #{resource.responsible.name}"
-    when "severity"
-      "A gravidade da(o) #{t_enum(resource, :title)} foi atualizada: #{t_enum(resource, :severity)}"
-    else
-      "Uma atualização foi realizada"
+    def call(resource:, changed_fields:)
+      case changed_fields.first
+      when "status"
+        "O status da(o) #{t_model(resource)} foi atualizado: #{t_enum(resource, :status)}"
+      when "area"
+        "A area da(o) #{t_model(resource)} foi atualizada: #{t_enum(resource, :area)}"
+      when "responsible_id"
+        "O responsável da(o) #{t_model(resource)} foi atualizado: #{resource.responsible.name}"
+      when "severity"
+        "A gravidade da(o) #{t_model(resource)} foi atualizada: #{t_enum(resource, :severity)}"
+      else
+        "Uma atualização foi realizada"
+      end
     end
   end
 end
--- app/helpers/translation_helper.rb
+++ app/helpers/translation_helper.rb
@@ -6,4 +6,9 @@ module TranslationHelper
     model = record.class.model_name.i18n_key
     I18n.t("activerecord.attributes.#{model}.#{attribute}.#{value}", locale: :'pt-BR', default: value.to_s.humanize)
   end
+
+  def t_model(record)
+    model_key = record.class.model_name.i18n_key
+    I18n.t("activerecord.models.#{model_key}", locale: :'pt-BR', default: model_key.to_s.humanize)
+  end
 end
